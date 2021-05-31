4c1dc20f8820bd2852ec84c3e540b4b7e51bfa52
Bryan-Joe123 <75519189+Bryan-Joe123@users.noreply.github.com>
2021-05-31 15:59:02 +0400

    Add files via upload

--- Scripts/database.js
+++ Scripts/database.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-apiKey: "AIzaSyAkeVE0dzVLZIWpVhtEotwH99oFyuy2lgQ",
-authDomain: "pet-game-11197.firebaseapp.com",
-databaseURL: "https://pet-game-11197-default-rtdb.firebaseio.com",
-projectId: "pet-game-11197",
-storageBucket: "pet-game-11197.appspot.com",
-messagingSenderId: "144122950913",
-appId: "1:144122950913:web:d697b13c9e091522cd1700"
+apiKey: "AIzaSyBv63TdDPFwjt4i03b6vzfXwzfa7jeA7Ww",
+authDomain: "virtual-pet-a1445.firebaseapp.com",
+databaseURL: "https://virtual-pet-a1445-default-rtdb.firebaseio.com",
+projectId: "virtual-pet-a1445",
+storageBucket: "virtual-pet-a1445.appspot.com",
+messagingSenderId: "745145606408",
+appId: "1:745145606408:web:e9bb6248e47407ea518cbd"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
